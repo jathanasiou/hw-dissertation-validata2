fac247c2d405be25f217557e244ebb35fc19f3b7
jathanasiou <jathanasiou@users.noreply.github.com>
2020-03-04 22:33:02 +0000

    adjust RDFShape API calls

--- src/utils/validationHelper.js
+++ src/utils/validationHelper.js
@@ -1,7 +1,7 @@
 import qs from 'query-string';
 
 
-const API = 'http://rdfshape.weso.es:8080/api/';
+const API = 'http://rdfshape.weso.es:8080/api';
 
 async function getFormats() {
   const requestEndpoint = qs.stringifyUrl({
@@ -11,18 +11,24 @@ async function getFormats() {
 }
 
 async function validate(schema, rdf) {
-  const requestEndpoint = qs.stringifyUrl({
-    url: `${API}/validate`,
-    query: {
-      dataFormat: 'turtle',
-      schemaFormat: 'ShExC',
-      data: rdf,
-      schema,
-      shapeMapFormat: 'compact',
-      "shape-map": "<genid-c7a50d6e828f4595a195f9c67304f6d2-0AE87CB066C8D9CD43117E0C5A3090F0>@<DatasetMinimum>",
-    },
-  });
-  return (await fetch(requestEndpoint, {method: 'GET'})).text();
+  const formData = new FormData();
+  // formData.append('activeTab', '#dataTextArea');
+  // formData.append('dataFormatTextArea', 'TURTLE');
+  // formData.append('schemaFormatTextArea', 'ShExC');
+  // formData.append('shapeMapActiveTab', '#shapeMapTextArea');
+  // formData.append('shapeMapFormatTextArea', 'Compact');
+  // formData.append('activeSchemaTab', '#schemaTextArea');
+  formData.append('dataFormat', 'TURTLE');
+  formData.append('data', rdf);
+  formData.append('schemaEmbedded', false);
+  formData.append('schemaFormat', 'ShExC');
+  formData.append('schema', schema);
+  formData.append('shapeMapFormat', 'ShExC');
+  formData.append('shapeMap', '<genid-c7a50d6e828f4595a195f9c67304f6d2-0AE87CB066C8D9CD43117E0C5A3090F0>@<DatasetMinimum>');
+  formData.append('schemaEngine', 'ShEx');
+  formData.append('triggerMode', 'shapeMap');
+
+  return (await fetch(`${API}/schema/validate`, { method: 'POST', body: formData })).json();
 }
 
 export {
