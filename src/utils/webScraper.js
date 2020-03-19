3bbc6f8d123f80597fb37928d994045d50f56ce3
jathanasiou <jathanasiou@users.noreply.github.com>
2020-03-19 21:24:21 +0000

    better error handling for scraping service

--- src/utils/webScraper.js
+++ src/utils/webScraper.js
@@ -3,12 +3,21 @@ import qs from 'query-string';
 
 const crawlerService = 'http://lxbisel.macs.hw.ac.uk:8080';
 
-export default async (rdfResource) => (await (await fetch(
-  qs.stringifyUrl({
-    url: `${crawlerService}/scraper/getRDF`,
-    query: {
-      output: 'turtle',
-      url: rdfResource,
-    },
-  }),
-)).json()).rdf[0];
+export default async (rdfResource) => {
+  const response = (await fetch(
+    qs.stringifyUrl({
+      url: `${crawlerService}/scraper/getRDF`,
+      query: {
+        output: 'turtle',
+        url: rdfResource,
+      },
+    }),
+  ));
+  const content = await response.json();
+
+  if (!response.ok || (typeof content.rdf === 'undefined') || (!content.rdf.length)) {
+    throw new Error(`Web resource "${rdfResource}" has no data or is unreachable.`);
+  }
+
+  return content.rdf[0];
+};
